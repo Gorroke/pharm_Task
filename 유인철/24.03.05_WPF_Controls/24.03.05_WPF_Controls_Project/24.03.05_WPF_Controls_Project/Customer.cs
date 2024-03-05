7699dabfb78ae72543f5138bcaf9492e39d1a61c
In-Cheol You <129019331+dlscjf628@users.noreply.github.com>
2024-03-05 10:08:29 +0900

    24.03.05 Project Complete

---
+++ 유인철/24.03.05_WPF_Controls/24.03.05_WPF_Controls_Project/24.03.05_WPF_Controls_Project/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24._03._05_WPF_Controls_Project
+{
+    internal class Customer
+    {
+        public string Name { get; set; }
+        public string Sex { get; set; }
+        public string Date_of_birth { get; set; }
+
+    }
+}
--- 유인철/24.03.05_WPF_Controls/24.03.05_WPF_Controls_Project/24.03.05_WPF_Controls_Project/MainWindow.xaml.cs
+++ 유인철/24.03.05_WPF_Controls/24.03.05_WPF_Controls_Project/24.03.05_WPF_Controls_Project/MainWindow.xaml.cs
@@ -20,10 +20,19 @@ namespace _24._03._05_WPF_Controls_Project
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Customer> CustomersList = new List<Customer>();
         public MainWindow()
         {
             InitializeComponent();
+            Dummy_Data_Create();
+            datagrid.ItemsSource = CustomersList;
         }
 
+        public void Dummy_Data_Create()
+        {
+            CustomersList.Add(new Customer() { Name = "유인철", Sex = "남성", Date_of_birth = "990628" });
+            CustomersList.Add(new Customer() { Name = "김기동", Sex = "남성", Date_of_birth = "990506" });
+            CustomersList.Add(new Customer() { Name = "최원민", Sex = "남성", Date_of_birth = "000109" });
+        }
     }
 }
