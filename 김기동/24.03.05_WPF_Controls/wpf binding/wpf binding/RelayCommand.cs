3fdcbeaae4ebbe5e6a0f06307d1657292bc53cf9
kimgidong1 <127929815+kimgidong1@users.noreply.github.com>
2024-03-07 17:50:10 +0900

    wpf 0307
    연습

---
+++ 김기동/24.03.05_WPF_Controls/wpf binding/wpf binding/RelayCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace wpf_binding
+{
+    internal class RelayCommand:ICommand
+    {
+        private readonly Action<object> _execute;
+        private readonly Predicate<object> _canExecute;
+        public RelayCommand(Action<object> execute,Predicate<object> canExecute=null) 
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute(parameter);
+        }
+    }
+}
