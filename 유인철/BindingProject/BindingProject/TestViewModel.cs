91b18005edbef49862992fb971907d77b416bd2e
In-Cheol You <129019331+dlscjf628@users.noreply.github.com>
2024-03-07 11:16:29 +0900

    Binding Project

---
+++ 유인철/BindingProject/BindingProject/TestViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BindingProject
+{
+    internal class TestViewModel : INotifyPropertyChanged
+    {
+        string testtext;
+        public string TestText
+        {
+            get { return testtext; }
+            set
+            {
+                testtext = value;
+                OnPropertyChanged("TestText");
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
