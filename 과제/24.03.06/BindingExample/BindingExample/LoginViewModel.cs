9639946af93db5e18bdd29ce05d3ad844d0cbb87
In-Cheol You <129019331+dlscjf628@users.noreply.github.com>
2024-03-07 15:38:03 +0900

    Add files via upload

---
+++ 과제/24.03.06/BindingExample/BindingExample/LoginViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace BindingExample
+{
+    internal class LoginViewModel : INotifyPropertyChanged
+    {
+        private string id;
+        public string ID
+        {
+            get { return id; }
+            set
+            {
+                id = value;
+                OnPropertyChanged(nameof(ID));
+            }
+        }
+
+        private string pw;
+        public string PW
+        {
+            get { return pw; }
+            set
+            {
+                pw = value;
+                OnPropertyChanged(nameof(PW));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+        public ICommand CMD_Login { get; set; }
+
+        public LoginViewModel()
+        {
+            CMD_Login = new RelayCommand(Login);
+        }
+
+        private void Login(object sender)
+        {
+        }
+
+    }
+}
